b6843df2e16b0215a01001503762dfce481f1248
divyansh351 <divyanshgupta351@gmail.com>
2023-10-22 21:14:59 +0530

    added jwt auth

--- routes/donor.js
+++ routes/donor.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const Donor = require('../controllers/donor')
 const router = express.Router();
-const { isDonorLoggedIn } = require('../middleware')
+const { checkToken } = require('../middleware')
 
 router.route('/register')
     .post(Donor.registerDonor)
 router.route('/verify')
     .post(Donor.verifyDonor)
 router.route('/view')
-    .get(isDonorLoggedIn, Donor.viewProfile)
+    .get(checkToken, Donor.viewProfile)
 
 module.exports = router
